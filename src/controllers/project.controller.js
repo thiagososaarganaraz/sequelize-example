5eb0a2f00e76e0c7c469868998c180d5a8ccda7f
Thiago <ts.facebook.ts@gmail.com>
2023-01-03 00:42:48 -0300

    Added the CRUD for Projects

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -2,9 +2,28 @@ import { Project } from "../models/Project.js";
 
 const getProjects = async (req, res) => {
   try {
-    throw new Error("query failed");
+    //Funcion de sequelize para traer todos los datos
     const projects = await Project.findAll();
     res.json(projects);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const getProject = async (req, res) => {
+  try {
+    //Traemos solo uno pasandole el ID desde params
+    const { id } = req.params;
+    const project = await Project.findOne({
+      where: {
+        id,
+      },
+    });
+
+    //Si no existen datos devolver un 404 not found
+    if (!project)
+      return res.status(404).json({ message: "Project does not exist" });
+    res.json(project);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -13,6 +32,7 @@ const getProjects = async (req, res) => {
 const createProject = async (req, res) => {
   const { name, priority, description } = req.body;
 
+  //Creamos pasando name description y priority
   try {
     const newProject = await Project.create({
       name,
@@ -26,4 +46,33 @@ const createProject = async (req, res) => {
   }
 };
 
-export { getProjects, createProject };
+const updateProject = async (req, res) => {
+  const { id } = req.params;
+  const { name, priority, description } = req.body;
+
+  //Editamos con findByPk que recibe el Id del dato a modificar
+  const project = await Project.findByPk(id);
+  project.name = name;
+  project.priority = priority;
+  project.description = description;
+  console.log(project);
+
+  res.send("updating");
+};
+
+const deleteProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+    //Metodo de sequelize para eliminar datos
+    await Project.destroy({
+      where: {
+        id,
+      },
+    });
+    res.send(204);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { getProjects, getProject, createProject, updateProject, deleteProject };
--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import {
   createProject,
+  deleteProject,
+  getProject,
   getProjects,
+  updateProject,
 } from "../controllers/project.controller.js";
 
 export const router = Router();
 
 router.get("/projects", getProjects);
 router.post("/projects", createProject);
-router.put("/projects/:id");
-router.delete("/projects/:id");
-router.get("/projects/:id");
+router.put("/projects/:id", updateProject);
+router.delete("/projects/:id", deleteProject);
+router.get("/projects/:id", getProject);
 
 export default router;
